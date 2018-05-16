26c1f4adb9b55056f1d99c4d7a117bebc75831a0
denis96z <zinoviev.denis@yandex.ru>
2018-05-16 02:04:34 -0700

    NGINX config: proxy and static.

--- ask_zinovyev/ask_zinovyev_app/management/commands/createquestions.py
+++ ask_zinovyev/ask_zinovyev_app/management/commands/createquestions.py
@@ -7,11 +7,19 @@ from ask_zinovyev_app.models import Question, User
 class Command(BaseCommand):
     help = 'Adds random data to database'
 
+    def add_arguments(self, parser):
+        parser.add_argument('num-questions', nargs=1, type=int)
+
     def handle(self, *args, **options):
+        num_questions = options['num-questions'][0]
+        if num_questions <= 0:
+            self.stdout.write('Number of questions (num-questions) is expected to be greater than 0.')
+            return
         users = User.objects.all()
-        for i in range(10000):
+        for i in range(num_questions):
             question = Question()
             question.title = "Вопрос №" + str(i)
             question.description = "Пояснение к вопросу"
             question.user = random.choice(users)
             question.save()
+        self.stdout.write('Successfully created {0} questions.'.format(num_questions))
