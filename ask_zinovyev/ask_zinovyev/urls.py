0e5c0c7e032fa560aea907e02d3cbf0986e09cde
denis96z <zinoviev.denis@yandex.ru>
2018-05-15 18:32:26 -0700

    Url patterns fixed to work with gunicorn.

--- ask_zinovyev/ask_zinovyev/urls.py
+++ ask_zinovyev/ask_zinovyev/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 from django.conf import settings
@@ -22,4 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('ask_zinovyev_app.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
